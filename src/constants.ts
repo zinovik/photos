8dae59975aefdcc90a68c2d0e84d56ef718716ab
Max Zinovik <zinovik@gmail.com>
2023-12-29 18:13:46 +0100

    update url

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,7 @@ export const PARAMETER_FILE = 'file';
 const baseUrl =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:5000'
-    : 'https://raw.githubusercontent.com/zinovik/gallery-data/main';
+    : 'https://storage.googleapis.com/zinovik-gallery';
 
 export const ALBUMS_URL = `${baseUrl}/albums.json`;
 export const FILES_URL = `${baseUrl}/files.json`;
