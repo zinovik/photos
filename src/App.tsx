255f1292202a1b731d5777fe7a1c74dcbbb59b83
Max Zinovik <zinovik@gmail.com>
2024-01-22 20:30:48 +0100

    add google auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import { MainRouter } from './routers/MainRouter';
 import { BackToTop } from './components/BackToTop';
 
 export const App = () => {
   return (
     <div className="App">
-      <Routes>
-        <Route path="*" element={<MainRouter />} />
-      </Routes>
-      <BackToTop />
+      <GoogleOAuthProvider clientId="306312319198-u9h4e07khciuet8hnj00b8fvmq25rlj0.apps.googleusercontent.com">
+        <Routes>
+          <Route path="*" element={<MainRouter />} />
+        </Routes>
+        <BackToTop />
+      </GoogleOAuthProvider>
     </div>
   );
 };
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { CredentialResponse, GoogleLogin } from '@react-oauth/google';
 import { File } from '../components/File';
 import { Markdown } from '../components/Markdown';
 import { AlbumWithFiles } from '../types';
@@ -10,6 +11,26 @@ interface Props {
 
 export const HomePage = ({ albumsWithFiles }: Props) => {
   if (albumsWithFiles.length === 0) return <>⏳ Loading...</>;
+
+  const API_URL = 'https://storage-json-editor-api-wniawguk3a-uc.a.run.app';
+
+  const googleSignInHandler = async (
+    credentialResponse: CredentialResponse
+  ) => {
+    console.log(credentialResponse);
+
+    const response = await fetch(`${API_URL}/auth/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ token: credentialResponse.credential }),
+    });
+
+    const token = await response.json();
+
+    console.log(token);
+  };
 
   return (
     <main>
@@ -26,6 +47,12 @@ export const HomePage = ({ albumsWithFiles }: Props) => {
           <Markdown text={album.text} />
         </div>
       ))}
+      <GoogleLogin
+        onSuccess={googleSignInHandler}
+        onError={() => {
+          console.error('Login Failed');
+        }}
+      />
     </main>
   );
 };
